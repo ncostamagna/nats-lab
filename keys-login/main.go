687bf9133ef2b3d0d55262a5410fe7cafa01d32a
Nahue <nlcostamagna@gmail.com>
2025-06-23 23:41:29 -0300

    key values

---
+++ keys-login/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nkeys"
+)
+
+type Message struct {
+	Content string `json:"content"`
+}
+
+func main() {
+	// Connect to the NATS server in Minikube
+
+	godotenv.Load()
+	natsURL := os.Getenv("NATS_URL")
+	seed := os.Getenv("NATS_SEED")
+	kp, _ := nkeys.FromSeed([]byte(seed))
+	pub, _ := kp.PublicKey()
+	log.Println("NATS_URL: ", natsURL)
+	log.Println("NATS_SEED: ", seed)
+	log.Println("kp: ", kp)
+
+	nc, err := nats.Connect("nats://localhost:4222", nats.Nkey(pub, func(data []byte) ([]byte, error) {
+		return kp.Sign(data)
+	}))
+	log.Println("NATS_URL: ", natsURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	sub, err := nc.Subscribe("hello.world", func(msg *nats.Msg) {
+		log.Printf("Received message: %s", string(msg.Data))
+		msg.Respond([]byte("Ahoy there!"))
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sub.Unsubscribe()
+
+	err = PublishMessage(nc, "hello.world", "Hello from Go!")
+	if err != nil {
+		log.Printf("Error publishing message: %v", err)
+	}
+
+	log.Println("Connected to NATS server. Press Ctrl+C to exit.")
+	
+	http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var message Message
+		if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		// Publish message to NATS
+		if err := PublishMessage(nc, "hello.world", message.Content); err != nil {
+			http.Error(w, "Failed to publish message", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Message sent successfully"))
+	})
+
+	// Start HTTP server
+	log.Println("Starting HTTP server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// PublishMessage publishes a message to a NATS subject
+func PublishMessage(nc *nats.Conn, subject string, message string) error {
+	return nc.Publish(subject, []byte(message))
+}
